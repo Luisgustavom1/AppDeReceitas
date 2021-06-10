8727d534ce87075fdf0e6957bf329e9cb065ad15
Luis Gustavo Macedo <luisgustavomacedo13@gmail.com>
2021-06-10 12:50:17 -0300

    Modal do random pronto

--- script.js
+++ script.js
@@ -1,47 +1,105 @@
 let cards = document.querySelectorAll('.cards')
 
-fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+function getMeal(){
+    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
     .then(response => response.json())
     .then(data => init(data))
+}
+
 
 function init(resultFromAPI){
-    addCard(resultFromAPI)
+    CreateNewMealRandom.addCard(resultFromAPI)
+    modalOverlayVerReceita.modalReceitaRandom(resultFromAPI)
+
+    var testando = resultFromAPI.meals.map(function(obj){
+        return Object.keys(obj).map(function(key){
+            return obj[key]
+        })
+    })
+    console.log(testando)
 }
 
-function createCard(response){
-    const html = `
-        <div class="cards">
-            <footer>
-                <p id="titulo">${response.meals[0].strMeal}</p>
-                <button id="verReceita">Ver receita</button>
-            </footer>
-        </div>
-    `
-    return html
-} 
-function addCard(response){
-    const section = document.querySelector('.sectionCards')
-    const div = document.createElement('div')
+class CreateNewMeal {
+    addCard(response){
+        const section = document.querySelector('.sectionCards')
+        const cards = document.querySelector('.cards1')
 
-    div.innerHTML = createCard(response)
-    section.appendChild(div)
+        cards.innerHTML = this.createCard(response)
+        section.appendChild(cards)
 
-    createCard(response)
-    addImage(response)
+        this.createCard(response)
+        this.addImage(response)  
+    }
+
+    createCard(response){
+            const html = `     
+                    <footer>
+                        <p id="titulo">${response.meals[0].strMeal}</p>
+                        <button id="verReceita" onclick='modalOverlayVerReceita.openModalVerReceita()'>Ver receita</button>
+                    </footer>
+            `
+            return html
+    }
+
+    
+    addImage(response){    
+        let style = document.createElement('style')
+        style.textContent = `
+            .cards1{
+                background-image: url(${response.meals[0].strMealThumb})
+            }
+            `
+            cards[0].appendChild(style)
+    }
 }
 
-function addImage(response){
-    console.log(response)
+class SetarLocalStorage {
+    get(){
+        return JSON.parse(localStorage.getItem('Meals'))
+    }
+    set(meal){
+        localStorage.setItem('Meals', JSON.stringify(meal))
+    }
+}
 
-    let style = document.createElement('style')
-    style.textContent = `
-        .cards{
-            background-image: url(${response.meals[0].strMealThumb})
+class ModalOverlayVerReceita {
+    openModalVerReceita(){
+        document.querySelector('.modal-overlay').classList.add('active')
+    }
+
+    modalReceitaRandom(response){
+        document.querySelector('.headerOverlay').innerHTML = response.meals[0].strMeal
+
+        const responseArr = response.meals.map(function(obj){
+            return Object.keys(obj).map(function(key){
+                return obj[key]
+            })
+        })
+
+        for(var c = 9; c < 28; c++){
+            if(responseArr[0][c] == ""){
+                console.log('NAO FAZ NADAAAAAAAAAAAAAAAAAAAAAAA')
+            }else{
+                const ul = document.querySelector('.lista')
+                const lis = document.createElement('li')
+
+                lis.innerHTML = `${responseArr[0][c+20]}  ${responseArr[0][c]}`
+
+                ul.appendChild(lis)
+            }
         }
-        `
-        cards[0].appendChild(style)
+
+        document.querySelector('.modal-overlay img').setAttribute('src', responseArr[0][6])
+        document.querySelector('.ModoDePreparo').innerHTML = responseArr[0][5]
+    }
 }
 
+const CreateNewMealRandom = new CreateNewMeal()
+const CreateLocalStorage = new SetarLocalStorage()
+const modalOverlayVerReceita = new ModalOverlayVerReceita()
 
+window.addEventListener('onload', getMeal())
 
-document.querySelector('.addReceita').addEventListener('click', init)
+document.querySelectorAll('#verReceita').forEach(function(verReceita){
+    verReceita.addEventListener('click', (e) => console.log(e))
+})
