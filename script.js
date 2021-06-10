b470df2df1a8e785a21c62e6c9dd56fa85b8c84d
Luis Gustavo Macedo <luisgustavomacedo13@gmail.com>
2021-06-10 08:09:22 -0300

    Começo do script

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
-fetch('https://www.themealdb.com/api/json/v1/1/random.php/')
+let cards = document.querySelectorAll('.cards')
+
+fetch('https://www.themealdb.com/api/json/v1/1/random.php')
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => init(data))
+
+function init(resultFromAPI){
+    addCard(resultFromAPI)
+}
+
+function createCard(response){
+    const html = `
+        <div class="cards">
+            <footer>
+                <p id="titulo">${response.meals[0].strMeal}</p>
+                <button id="verReceita">Ver receita</button>
+            </footer>
+        </div>
+    `
+    return html
+} 
+function addCard(response){
+    const section = document.querySelector('.sectionCards')
+    const div = document.createElement('div')
+
+    div.innerHTML = createCard(response)
+    section.appendChild(div)
+
+    createCard(response)
+    addImage(response)
+}
+
+function addImage(response){
+    console.log(response)
+
+    let style = document.createElement('style')
+    style.textContent = `
+        .cards{
+            background-image: url(${response.meals[0].strMealThumb})
+        }
+        `
+        cards[0].appendChild(style)
+}
+
+
+
+document.querySelector('.addReceita').addEventListener('click', init)
